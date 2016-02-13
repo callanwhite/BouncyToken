77b919b4781dabc334ec573b43b3bf36afa62a60
Callan White <me@callanwhite.co.uk>
2016-02-13 23:02:01 +0000

    utilities and generated gitignore

---
+++ BouncyToken/Utility/Password.cs
@@ -0,0 +1,18 @@
+﻿using Org.BouncyCastle.OpenSsl;
+
+namespace BouncyToken.Utility
+{
+	public class Password : IPasswordFinder
+	{
+		char[] password = new char[0];
+		public Password(string password)
+		{
+			this.password = password.ToCharArray();
+		}
+
+		public char[] GetPassword()
+		{
+			return password;
+		}
+	}
+}
---
+++ BouncyToken/Utility/Utility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BouncyToken.Utility
+{
+	public class Utility
+	{
+		static string Base64UrlEncode(byte[] input)
+		{
+			string result = Convert.ToBase64String(input);
+
+			result = result.Split('=')[0];
+			result = result.Replace('+', '-');
+			result = result.Replace('/', '_');
+
+			return result;
+		}
+
+		static byte[] Base64UrlDecode(string input)
+		{
+			input.Replace('-', '+');
+			input.Replace('_', '/');
+
+			switch (input.Length % 4)
+			{
+				case 0:
+					break;
+				case 2:
+					input += "==";
+					break;
+				case 3:
+					input += "=";
+					break;
+				default:
+					throw new Exception("Invalid base64 string");
+			}
+
+			return Convert.FromBase64String(input);
+		}
+
+		
+	}
+}
